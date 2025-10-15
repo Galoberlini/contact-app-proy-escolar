c0bffa798912851e98a070713bdc9eb19091ced6
Galo <galoberlini@gmail.com>
2025-10-15 11:09:44 -0300

    Feature: basic routes and pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './pages/login-page/login-page';
+import { ContactsPage } from './pages/contacts-page/contacts-page';
+import { ContactsDetailsPage } from './pages/contacts-details-page/contacts-details-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: "login",
+        component: LoginPage
+    },
+
+    {
+        path: "",
+        component: ContactsPage
+    },
+
+    {
+        path: "contacts/:id",
+        component: ContactsDetailsPage
+    },
+];
---
+++ src/app/pages/contacts-details-page/contacts-details-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts-details-page',
+  imports: [],
+  templateUrl: './contacts-details-page.html',
+  styleUrl: './contacts-details-page.scss'
+})
+export class ContactsDetailsPage {
+
+}
