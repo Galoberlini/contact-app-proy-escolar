e5d0a37325cfbe70415aaed86dcf1a1abf90d34c
Galo <galoberlini@gmail.com>
2025-10-19 19:00:25 -0300

    Feature: proper routing, guards and sweetaler2 implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,31 +4,49 @@ import { ContactsPage } from './pages/contacts-page/contacts-page';
 import { ContactsDetailsPage } from './pages/contacts-details-page/contacts-details-page';
 import { Logged } from './layouts/logged/logged';
 import { RegisterPage } from './pages/register-page/register-page';
+import { onlyPublicUserGuard } from './guards/only-public-user-guard';
+import { onlyLoggedUserGuard } from './guards/only-logged-user-guard';
+import { NewEditContact } from './pages/new-edit-contact/new-edit-contact';
+import { GroupsPage } from './pages/groups/groups';
 
 export const routes: Routes = [
 
     {
         path: "login",
-        component: LoginPage
+        component: LoginPage,
+        canActivate: [onlyPublicUserGuard]
     },
 
     {
         path: "register",
-        component: RegisterPage
-    },
-
-    {
-        path: "contacts/:id",
-        component: ContactsDetailsPage
+        component: RegisterPage,
+        canActivate: [onlyPublicUserGuard]
     },
     {
         path: "",
         component: Logged,
+        canActivateChild: [onlyLoggedUserGuard],
         children: [
             {
                 path: "",
                 component: ContactsPage
             },
+            {
+                path: "contacts/new",
+                component: NewEditContact
+            },
+            {
+                path: "contacts/:idContacto",
+                component: ContactsDetailsPage
+            },
+            {
+                path: "contacts/:idContacto/edit",
+                component: NewEditContact
+            },
+            {
+                path: "groups",
+                component: GroupsPage
+            }
         ]
     },
 ];
--- src/app/layouts/logged/logged.ts
+++ src/app/layouts/logged/logged.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import Swal, { SweetAlertResult } from 'sweetalert2';
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-logged',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './logged.html',
   styleUrl: './logged.scss'
 })
 export class Logged {
+
+  authService = inject(AuthService);
+
+  openLogoutModal() {
+    Swal.fire({
+      title: '¿Desea cerrar sesión?',
+      showDenyButton: true,
+      showCancelButton: true,
+      showConfirmButton: false,
+      cancelButtonText: 'Cancelar',
+      denyButtonText: 'Cerrar sesión'
+    }).then((result: SweetAlertResult) => {
+      if (result.isDenied) {
+        this.authService.logout();
+      }
+    });
+  }
 }
